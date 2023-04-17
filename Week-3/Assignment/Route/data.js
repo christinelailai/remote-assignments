dd41fe690f19fdb21cdd7e56175a53b5164c6a8e
christinelailai <laipinhui.laipinhui@outlook.com>
2023-04-17 19:43:42 +0800

    assign week3 assignment

---
+++ Week-3/Assignment/Route/data.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+
+
+router.get(`/`,(req,res) => {
+    let text = '';
+    if(!req.query.number) {
+        text='Lack of Parameter'
+
+    } else if (Number.isNaN(req.query.number)|| req.query.number <= 0 || req.query.number % 1 !== 0) {
+        text='Wrong Parameter'
+    }  else {
+        text=((parseInt(req.query.number) + 1) * req.query.number/2).toString();
+    }
+    res.render('index.ejs',{text: text})
+});
+
+
+
+
+
+module.exports = router;
