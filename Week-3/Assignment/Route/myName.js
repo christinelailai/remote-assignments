d8d0ad0dbaa93765b9e53af560df885e14e23054
christinelailai <laipinhui.laipinhui@outlook.com>
2023-04-17 19:34:55 +0800

    Revert "reassign week3 assignment"
    This reverts commit 4d1d2b73bef805e045e56a3265353160d0c7ef56.

--- Week-3/Assignment/Route/myName.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/',(req,res) => {
-    const name = req.cookies.name;
-    res.render("myName.ejs",{name: name})
-})
-
-module.exports = router;
